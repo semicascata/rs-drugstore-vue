4381e22a85a6a994ea1c7a78c7d533c42433908c
r. drt <51922521+semicascata@users.noreply.github.com>
2020-10-10 20:07:31 -0300

    api url

--- src/_helpers/url.js
+++ src/_helpers/url.js
@@ -1,4 +1,5 @@
 // API URL
-const URL = 'http://localhost:3000/shelter/v1'
+// const URL = 'http://localhost:3000/shelter/v1';
+const URL = "https://rs-drugstore-expressjs.herokuapp.com/shelter/v1";
 
-export default URL
+export default URL;
