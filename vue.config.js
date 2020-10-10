214dc547a3c8bf5c4764750a6ec056c6bf0fdf59
r. drt <51922521+semicascata@users.noreply.github.com>
2020-10-10 20:11:31 -0300

    vue.config & deploy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../src/public"),
-  devServer: {
-    proxy: {
-      "/shelter/v1": {
-        target: "http://localhost:3000"
-      }
-    }
-  }
+  outputDir: path.resolve(__dirname, "./src/dist"),
 };
